85f9092f37f67d051c2b06f647a0f6e8302e333b
Jayon Kim <jpkim921@gmail.com>
2018-08-03 01:57:41 +0000

    automatically backed up by learn

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,4 +2,8 @@ class Patient < ActiveRecord::Base
   has_many :doctors, through: :appointments
   has_many :appointments
 
+  def number_of_appointments
+    self.appointments.count    
+  end
+
 end
